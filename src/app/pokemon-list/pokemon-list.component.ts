9f98bbe21d77c42f13f90a74a834af11a78f5412
Tekarl <tekarlan2019@gmail.com>
2024-05-28 09:05:17 +0200

    routes added and pokemon-detail's logic implemented

--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+import { POKEMONS } from '../mock-pokemons';
+import { Pokemon } from '../pokemon';
+
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
   styles: ``
 })
 export class PokemonListComponent {
-
+  pokemon_list: Pokemon[] = POKEMONS;
 }
